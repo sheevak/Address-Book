32a15f58b823371ee5cc457488e6ec6cd0ba6c61
Sheeva Kavousian <sheevakavous@gmail.com>
2020-11-08 21:36:41 +0000

    Fixed small errors.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 function AddingContact (form) {
-  //Creating a new object of the new contact's details
-  let contactName = `${form.name.value}${form.surname.value}`.toUpperCase();
 
-  counter = 0;
-
-    $('input').each(function(){
-          if(!$(this).val()){
-            $(this).addClass("error");
-            counter += 1;
-          } else {
-            $(this).removeClass("error");
-          }
+  let counter = 0;
+  //Highlighting fields which have been left blank
+  $('.add').each(function(){
+    console.log($(this));
+    console.log($(this).val());
+    if($(this).val() == "" ){
+      $(this).addClass("error");
+      //console.log($(this).val());
+      counter += 1;
+    } else {
+      $(this).removeClass("error");
+      //console.log($(this).val());
+    }
   });
 
+  console.log(counter);
+  /*Testing if any fields have been left blank. If they have been the contact is not added
+    to the address book and an error message is returned.
+  */
   if (counter === 0){
+    //Creating a new object of the new contact's details
+    let contactName = `${form.name.value}${form.surname.value}`.toUpperCase();
     const newContact = {
       name: form.name.value,
       surname: form.surname.value,
@@ -44,7 +52,7 @@ function AddingContact (form) {
     //Alerting the user that the contact had been added
     let alert = $("<h5>");
     alert.text(`${newContact.name} ${newContact.surname} has been added to your contacts.`).addClass("alert");
-    $("form").after(alert);
+    $("#submit").after(alert);
 
     //Clearing the input fields
     form.name.value = ("");
@@ -57,12 +65,13 @@ function AddingContact (form) {
 
     //Alert the user that the contact info is not correct
     let errorAlert = $("<h5>");
-    errorAlert.text(`Contact not added! Please fill all fields.`).addClass("error-alert");
-    $("form").after(errorAlert);
+    errorAlert.text(`Contact not added! Please fill in all fields.`).addClass("error-alert");
+    $("#submit").after(errorAlert);
   }
 }
 
 function view(hide, show, front, back) {
+  //hiding and showing the different tabs
   $(hide).css("display", "none");
   $(show).css("display", "block");
   $(front).css("background-color", "#ffffff");
@@ -70,8 +79,7 @@ function view(hide, show, front, back) {
 };
 
 function deleteContact(contactId){
-  $( contactId ).remove();
-  // $(contactId).css("display", "none");
+  $(contactId).remove();
 }
 
 function search(form){
