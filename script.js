e9da9675249747e79982cd21cc5aaed27ce6eb39
Sheeva Kavousian <sheevakavous@gmail.com>
2020-11-08 21:11:59 +0000

    Added error message.

--- script.js
+++ script.js
@@ -2,44 +2,64 @@ function AddingContact (form) {
   //Creating a new object of the new contact's details
   let contactName = `${form.name.value}${form.surname.value}`.toUpperCase();
 
-  const newContact = {
-    name: form.name.value,
-    surname: form.surname.value,
-    phone: form.phone.value,
-    address: form.address.value,
-    city: form.city.value,
-    postcode: form.postcode.value
-  };
+  counter = 0;
 
-  //Creating a new div to display the new contact's details
-  const newDiv = $("<div>");
-  newDiv.attr('id', `${contactName}`).addClass("contact");
-  $(".grid").append(newDiv);
+    $('input').each(function(){
+          if(!$(this).val()){
+            $(this).addClass("error");
+            counter += 1;
+          } else {
+            $(this).removeClass("error");
+          }
+  });
 
-  //Creating new elements to display the new contact's details
-  let fullName = $("<p>");
-  fullName.text(`${newContact.name} ${newContact.surname}`).addClass("full");
-  let details = $("<p>");
-  details.html(`<strong>Address:</strong><br>${newContact.address}<br>${newContact.city}, ${newContact.postcode}<br><strong>Phone Number:</strong><br>${newContact.phone}`);
-  $(`#${contactName}`).append(fullName);
-  $(`#${contactName}`).append(details);
+  if (counter === 0){
+    const newContact = {
+      name: form.name.value,
+      surname: form.surname.value,
+      phone: form.phone.value,
+      address: form.address.value,
+      city: form.city.value,
+      postcode: form.postcode.value
+    };
 
-  //Creating a button element for deleting the contact
-  let deleteButton = $("<button>");
-  deleteButton.addClass("fas").addClass("fa-trash").attr("type", "button").attr("onClick", `deleteContact("#${contactName}")`);
+    //Creating a new div to display the new contact's details
+    const newDiv = $("<div>");
+    newDiv.attr('id', `${contactName}`).addClass("contact");
+    $(".grid").append(newDiv);
 
-  //Alerting the user that the contact had been added
-  let alert = $("<h5>");
-  alert.text(`${newContact.name} ${newContact.surname} has been added to your contacts.`).addClass("alert");
-  $("#submit").after(alert);
+    //Creating new elements to display the new contact's details
+    let fullName = $("<p>");
+    fullName.text(`${newContact.name} ${newContact.surname}`).addClass("full");
+    let details = $("<p>");
+    details.html(`<strong>Address:</strong><br>${newContact.address}<br>${newContact.city}, ${newContact.postcode}<br><strong>Phone Number:</strong><br>${newContact.phone}`);
+    $(`#${contactName}`).append(fullName);
+    $(`#${contactName}`).append(details);
 
-  //Clearing the input fields
-  form.name.value = ("");
-  form.surname.value = ("");
-  form.phone.value = ("");
-  form.address.value = ("");
-  form.city.value = ("");
-  form.postcode.value = ("");
+    //Creating a button element for deleting the contact
+    let deleteButton = $("<button>");
+    deleteButton.addClass("fas").addClass("fa-trash").attr("type", "button").attr("onClick", `deleteContact("#${contactName}")`);
+    $(`#${contactName}`).append(deleteButton);
+
+    //Alerting the user that the contact had been added
+    let alert = $("<h5>");
+    alert.text(`${newContact.name} ${newContact.surname} has been added to your contacts.`).addClass("alert");
+    $("form").after(alert);
+
+    //Clearing the input fields
+    form.name.value = ("");
+    form.surname.value = ("");
+    form.phone.value = ("");
+    form.address.value = ("");
+    form.city.value = ("");
+    form.postcode.value = ("");
+  } else {
+
+    //Alert the user that the contact info is not correct
+    let errorAlert = $("<h5>");
+    errorAlert.text(`Contact not added! Please fill all fields.`).addClass("error-alert");
+    $("form").after(errorAlert);
+  }
 }
 
 function view(hide, show, front, back) {
@@ -50,7 +70,8 @@ function view(hide, show, front, back) {
 };
 
 function deleteContact(contactId){
-  $(contactId).css("display", "none");
+  $( contactId ).remove();
+  // $(contactId).css("display", "none");
 }
 
 function search(form){
